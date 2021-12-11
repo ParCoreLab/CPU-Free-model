0e12de61ec0010d5ce170aff0ec7d8b8c8464156
Javid Baydamirli <jbaydamirli21@ku.edu.tr>
2021-12-11 19:20:29 +0300

    Added single GPU naive implementation from NVIDIA
    https://github.com/NVIDIA/multi-gpu/programming-models
    ./single_gpu/jacobi.cu

--- src/main.cu
+++ src/main.cu
@@ -1,6 +1,8 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include "../include/single-gpu-naive.cuh"
+
+int main(int argc, char* argv[]) {
+    std::cout << "Running Single GPU Naive version" << std::endl;
+    return init(argc, argv);
 }
