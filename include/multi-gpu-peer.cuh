495d68e4bfe8dcca5489e41f701fd2adbee18670
Javid Baydamirli <jbaydamirli21@ku.edu.tr>
2022-01-10 15:53:58 +0300

    Added and enabled multi-gpu version

---
+++ include/multi-gpu-peer.cuh
@@ -0,0 +1,6 @@
+#ifndef INC_2D_STENCIL_SINGLE_GPU_NAIVE_CUH
+#define INC_2D_STENCIL_SINGLE_GPU_NAIVE_CUH
+
+int init(int argc, char* argv[]);
+
+#endif //INC_2D_STENCIL_SINGLE_GPU_NAIVE_CUH
--- src/main.cu
+++ src/main.cu
@@ -1,6 +1,7 @@
 #include <iostream>
 
-#include "../include/single-gpu-naive.cuh"
+//#include "../include/single-gpu-naive.cuh"
+#include "../include/multi-gpu-peer.cuh"
 
 int main(int argc, char* argv[]) {
     std::cout << "Running Single GPU Naive version" << std::endl;
