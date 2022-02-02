26b365a00b41e7537195d7cd7e572f386022bbac
Ismayil Ismayilov <iismayilov4574@gmail.com>
2022-02-02 15:12:52 +0300

    Fix header

--- include/common.h
+++ include/common.h
@@ -1,156 +1,77 @@
-#include "../include/common.h"
+#ifndef INC_2D_STENCIL_COMMON_H
+#define INC_2D_STENCIL_COMMON_H
 
+#include <omp.h>
+#include <algorithm>
+#include <sstream>
+#include <string>
+
+typedef float real;
+
+typedef int (*initfunc_t)(int argc, char** argv);
+
+constexpr int MAX_NUM_DEVICES{32};
+constexpr real tol = 1.0e-4;
+const real PI{static_cast<real>(2.0 * std::asin(1.0))};
+
+template <typename T>
+T get_argval(char** begin, char** end, const std::string& arg, const T default_val) {
+    T argval = default_val;
+    char** itr = std::find(begin, end, arg);
+    if (itr != end && ++itr != end) {
+        std::istringstream inbuf(*itr);
+        inbuf >> argval;
+    }
+    return argval;
+}
 __global__ void initialize_boundaries(real* __restrict__ const a_new, real* __restrict__ const a,
                                       const real pi, const int offset, const int nx,
-                                      const int my_ny, const int ny) {
-    for (unsigned int iy = blockIdx.x * blockDim.x + threadIdx.x; iy < my_ny;
-         iy += blockDim.x * gridDim.x) {
-        const real y0 = sin(2.0 * pi * (offset + iy) / (ny - 1));
-        a[iy * nx + 0] = y0;
-        a[iy * nx + (nx - 1)] = y0;
-        a_new[iy * nx + 0] = y0;
-        a_new[iy * nx + (nx - 1)] = y0;
-    }
-}
-
+                                      const int my_ny, const int ny);
 __global__ void jacobi_kernel_single_gpu(real* __restrict__ const a_new,
                                          const real* __restrict__ const a,
                                          real* __restrict__ const l2_norm, const int iy_start,
-                                         const int iy_end, const int nx,
-                                         const bool calculate_norm) {
-    int iy = blockIdx.y * blockDim.y + threadIdx.y + iy_start;
-    int ix = blockIdx.x * blockDim.x + threadIdx.x + 1;
-    //    real local_l2_norm = 0.0;
-
-    if (iy < iy_end && ix < (nx - 1)) {
-        const real new_val = 0.25 * (a[iy * nx + ix + 1] + a[iy * nx + ix - 1] +
-                                     a[(iy + 1) * nx + ix] + a[(iy - 1) * nx + ix]);
-        a_new[iy * nx + ix] = new_val;
-
-        //        if (calculate_norm) {
-        //            real residue = new_val - a[iy * nx + ix];
-        //            local_l2_norm += residue * residue;
-        //        }
-    }
-    //    if (calculate_norm) {
-    //        atomicAdd(l2_norm, local_l2_norm);
-    //    }
-}
-
+                                         const int iy_end, const int nx, const bool calculate_norm);
 double single_gpu(const int nx, const int ny, const int iter_max, real* const a_ref_h,
-                  const int nccheck, const bool print) {
-    real* a;
-    real* a_new;
+                  const int nccheck, const bool print);
 
-    cudaStream_t compute_stream;
-    cudaStream_t push_top_stream;
-    cudaStream_t push_bottom_stream;
-    cudaEvent_t compute_done;
-    cudaEvent_t push_top_done;
-    cudaEvent_t push_bottom_done;
+#define noop
 
-    //    real* l2_norm_d;
-    //    real* l2_norm_h;
+#ifdef USE_NVTX
+#include <nvToolsExt.h>
 
-    int iy_start = 1;
-    int iy_end = (ny - 1);
+const uint32_t colors[] = {0x0000ff00, 0x000000ff, 0x00ffff00, 0x00ff00ff,
+                           0x0000ffff, 0x00ff0000, 0x00ffffff};
+const int num_colors = sizeof(colors) / sizeof(uint32_t);
 
-    CUDA_RT_CALL(cudaMalloc(&a, nx * ny * sizeof(real)));
-    CUDA_RT_CALL(cudaMalloc(&a_new, nx * ny * sizeof(real)));
-
-    CUDA_RT_CALL(cudaMemset(a, 0, nx * ny * sizeof(real)));
-    CUDA_RT_CALL(cudaMemset(a_new, 0, nx * ny * sizeof(real)));
-
-    // Set diriclet boundary conditions on left and right boarder
-    initialize_boundaries<<<ny / 128 + 1, 128>>>(a, a_new, PI, 0, nx, ny, ny);
-    CUDA_RT_CALL(cudaGetLastError());
-    CUDA_RT_CALL(cudaDeviceSynchronize());
-
-    CUDA_RT_CALL(cudaStreamCreate(&compute_stream));
-    CUDA_RT_CALL(cudaStreamCreate(&push_top_stream));
-    CUDA_RT_CALL(cudaStreamCreate(&push_bottom_stream));
-    CUDA_RT_CALL(cudaEventCreateWithFlags(&compute_done, cudaEventDisableTiming));
-    CUDA_RT_CALL(cudaEventCreateWithFlags(&push_top_done, cudaEventDisableTiming));
-    CUDA_RT_CALL(cudaEventCreateWithFlags(&push_bottom_done, cudaEventDisableTiming));
-
-    //    CUDA_RT_CALL(cudaMalloc(&l2_norm_d, sizeof(real)));
-    //    CUDA_RT_CALL(cudaMallocHost(&l2_norm_h, sizeof(real)));
-
-    CUDA_RT_CALL(cudaDeviceSynchronize());
-
-    if (print)
-        printf(
-            "Single GPU jacobi relaxation: %d iterations on %d x %d mesh with "
-            "norm "
-            "check every %d iterations\n",
-            iter_max, ny, nx, nccheck);
-
-    constexpr int dim_block_x = 32;
-    constexpr int dim_block_y = 32;
-    dim3 dim_grid((nx + dim_block_x - 1) / dim_block_x, (ny + dim_block_y - 1) / dim_block_y, 1);
-
-    int iter = 0;
-    bool calculate_norm = false;
-    //    real l2_norm = 1.0;
-
-    double start = omp_get_wtime();
-    PUSH_RANGE("Jacobi solve", 0)
-    while (iter < iter_max) {
-        //        CUDA_RT_CALL(cudaMemsetAsync(l2_norm_d, 0, sizeof(real), compute_stream));
-
-        CUDA_RT_CALL(cudaStreamWaitEvent(compute_stream, push_top_done, 0));
-        CUDA_RT_CALL(cudaStreamWaitEvent(compute_stream, push_bottom_done, 0));
-
-        //        calculate_norm = (iter % nccheck) == 0 || (print && ((iter % 100) == 0));
-        jacobi_kernel_single_gpu<<<dim_grid, {dim_block_x, dim_block_y, 1}, 0, compute_stream>>>(
-            a_new, a, nullptr, iy_start, iy_end, nx, calculate_norm);
-        CUDA_RT_CALL(cudaGetLastError());
-        CUDA_RT_CALL(cudaEventRecord(compute_done, compute_stream));
-
-        //        if (calculate_norm) {
-        //            CUDA_RT_CALL(cudaMemcpyAsync(l2_norm_h, l2_norm_d, sizeof(real),
-        //            cudaMemcpyDeviceToHost,
-        //                                         compute_stream));
-        //        }
-
-        // Apply periodic boundary conditions
-
-        CUDA_RT_CALL(cudaStreamWaitEvent(push_top_stream, compute_done, 0));
-        CUDA_RT_CALL(cudaMemcpyAsync(a_new, a_new + (iy_end - 1) * nx, nx * sizeof(real),
-                                     cudaMemcpyDeviceToDevice, push_top_stream));
-        CUDA_RT_CALL(cudaEventRecord(push_top_done, push_top_stream));
-
-        CUDA_RT_CALL(cudaStreamWaitEvent(push_bottom_stream, compute_done, 0));
-        CUDA_RT_CALL(cudaMemcpyAsync(a_new + iy_end * nx, a_new + iy_start * nx, nx * sizeof(real),
-                                     cudaMemcpyDeviceToDevice, compute_stream));
-        CUDA_RT_CALL(cudaEventRecord(push_bottom_done, push_bottom_stream));
-
-        //        if (calculate_norm) {
-        //            CUDA_RT_CALL(cudaStreamSynchronize(compute_stream));
-        //            l2_norm = *l2_norm_h;
-        //            l2_norm = std::sqrt(l2_norm);
-        //            if (print && (iter % 100) == 0) printf("%5d, %0.6f\n", iter, l2_norm);
-        //        }
-
-        std::swap(a_new, a);
-        iter++;
+#define PUSH_RANGE(name, cid)                              \
+    {                                                      \
+        int color_id = cid;                                \
+        color_id = color_id % num_colors;                  \
+        nvtxEventAttributes_t eventAttrib = {0};           \
+        eventAttrib.version = NVTX_VERSION;                \
+        eventAttrib.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE;  \
+        eventAttrib.colorType = NVTX_COLOR_ARGB;           \
+        eventAttrib.color = colors[color_id];              \
+        eventAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII; \
+        eventAttrib.message.ascii = name;                  \
+        nvtxRangePushEx(&eventAttrib);                     \
     }
-    POP_RANGE
-    double stop = omp_get_wtime();
+#define POP_RANGE nvtxRangePop();
+#else
+#define PUSH_RANGE(name, cid)
+#define POP_RANGE
+#endif
 
-    CUDA_RT_CALL(cudaMemcpy(a_ref_h, a, nx * ny * sizeof(real), cudaMemcpyDeviceToHost));
+#define CUDA_RT_CALL(call)                                                                  \
+    {                                                                                       \
+        cudaError_t cudaStatus = call;                                                      \
+        if (cudaSuccess != cudaStatus)                                                      \
+            fprintf(stderr,                                                                 \
+                    "ERROR: CUDA RT call \"%s\" in line %d of file %s failed "              \
+                    "with "                                                                 \
+                    "%s (%d).\n",                                                           \
+                    #call, __LINE__, __FILE__, cudaGetErrorString(cudaStatus), cudaStatus); \
+    }                                                                                       \
+    noop
 
-    CUDA_RT_CALL(cudaEventDestroy(push_bottom_done));
-    CUDA_RT_CALL(cudaEventDestroy(push_top_done));
-    CUDA_RT_CALL(cudaEventDestroy(compute_done));
-    CUDA_RT_CALL(cudaStreamDestroy(push_bottom_stream));
-    CUDA_RT_CALL(cudaStreamDestroy(push_top_stream));
-    CUDA_RT_CALL(cudaStreamDestroy(compute_stream));
-
-    //    CUDA_RT_CALL(cudaFreeHost(l2_norm_h));
-    //    CUDA_RT_CALL(cudaFree(l2_norm_d));
-
-    CUDA_RT_CALL(cudaFree(a_new));
-    CUDA_RT_CALL(cudaFree(a));
-    return (stop - start);
-}
+#endif  // INC_2D_STENCIL_COMMON_H
