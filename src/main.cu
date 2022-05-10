a88b287a176add8ebc23f9382ebf05eb130e228b
Javid Baydamirli <jbaydamirli21@ku.edu.tr>
2022-05-10 13:53:38 +0300

    Removed explicit length from std::array

--- src/main.cu
+++ src/main.cu
@@ -6,26 +6,29 @@
 #include "../include/baseline/single-threaded-copy.cuh"
 
 #include "../include/common.h"
-#include "../include/multi-gpu-peer.cuh"
+#include "../include/multi-stream/multi-gpu-peer-tiling.cuh"
+#include "../include/multi-stream/multi-gpu-peer.cuh"
 #include "../include/single-stream/multi-threaded-two-block-comm.cuh"
 #include "../include/single-stream/multi-threaded.cuh"
 #include "../include/single-stream/single-threaded-two-block-comm.cuh"
 #include "../include/single-stream/single-threaded.cuh"
 
+using std::make_pair;
+
 int main(int argc, char *argv[]) {
-    const std::array<std::pair<std::string, initfunc_t>, 9> versions{
-            std::make_pair("Single stream multi threaded (default)", SSMultiThreaded::init),
-            std::make_pair("Single stream multi threaded (two thread blocks communicate)",
-                           SSMultiThreadedTwoBlockComm::init),
-            std::make_pair("Single stream single threaded", SSSingleThreaded::init),
-            std::make_pair("Single stream single threaded (two thread blocks communicate)",
-                           SSSingleThreadedTwoBlockComm::init),
-            std::make_pair("Double stream multi threaded", MultiGPUPeer::init),
-            std::make_pair("Baseline Multi Threaded Copy", BaselineMultiThreadedCopy::init),
-            std::make_pair("Baseline Multi Threaded Copy Overlap",
-                           BaselineMultiThreadedCopyOverlap::init),
-            std::make_pair("Baseline Multi Threaded P2P", BaselineMultiThreadedP2P::init),
-            std::make_pair("Baseline Single Threaded Copy", BaselineSingleThreadedCopy::init)};
+    const std::array versions{
+        make_pair("Single stream multi threaded (default)", SSMultiThreaded::init),
+        make_pair("Single stream multi threaded (two thread blocks communicate)",
+                  SSMultiThreadedTwoBlockComm::init),
+        make_pair("Single stream single threaded", SSSingleThreaded::init),
+        make_pair("Single stream single threaded (two thread blocks communicate)",
+                  SSSingleThreadedTwoBlockComm::init),
+        make_pair("Double stream multi threaded", MultiGPUPeer::init),
+        make_pair("Baseline Multi Threaded Copy", BaselineMultiThreadedCopy::init),
+        make_pair("Baseline Multi Threaded Copy Overlap", BaselineMultiThreadedCopyOverlap::init),
+        make_pair("Baseline Multi Threaded P2P", BaselineMultiThreadedP2P::init),
+        make_pair("Baseline Single Threaded Copy", BaselineSingleThreadedCopy::init),
+        make_pair("Double stream multi threaded with Tiling", MultiGPUPeerTiling::init)};
 
     const int selection = get_argval<int>(argv, argv + argc, "-v", 0);
     const bool silent = get_arg(argv, argv + argc, "-s");
