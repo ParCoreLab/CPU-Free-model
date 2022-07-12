b041e2879a910ea4ec2c895f48f96f6f5870a859
Javid Baydamirli <jbaydamirli21@ku.edu.tr>
2022-07-12 16:39:30 +0300

    Swiched to nvcc 11.6
    The default in NVHPC is 11.0

--- scripts/modules.sh
+++ scripts/modules.sh
@@ -4,6 +4,6 @@ module load nvidia/nvhpc/22.3
 
 COMM_LIBS_PATH="$NVHPC_ROOT"/comm_libs
 
-export NVCC="$CC"c # sorry
+export NVCC="$NVHPC_ROOT"/cuda/bin/nvcc
 export MPI_HOME="$COMM_LIBS_PATH"/mpi
 export NVSHMEM_HOME="$COMM_LIBS_PATH"/nvshmem
