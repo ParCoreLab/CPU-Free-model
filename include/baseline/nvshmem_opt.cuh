96b0da9b3ba27238458b3f8c1f1f0ef4d6b1e272
Javid Baydamirli <jbaydamirli21@ku.edu.tr>
2022-07-25 12:56:12 +0300

    Moved "Baseline Nvshmem ported." to a separate branch
    This reverts commit 1ac148c91fedc078e815775ea17d468581652970.

--- include/baseline/nvshmem_opt.cuh
+++
@@ -1,9 +0,0 @@
-#ifndef INC_2D_STENCIL_BASELINE_NVSHMEM_OPT_CUH
-#define INC_2D_STENCIL_BASELINE_NVSHMEM_OPT_CUH
-
-#include "../common.h"
-
-namespace BaselineNvshmemOptimized{
-    int init(int argc, char **argv);
-}
-#endif //INC_2D_STENCIL_BASELINE_NVSHMEM_OPT_CUH
