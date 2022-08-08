8683c45eacaf4bc0b08588832256cdf15277f83d
Ismayil Ismayilov <iismayilov4574@gmail.com>
2022-08-08 16:55:46 +0300

    Add script for sbatch

---
+++ CG/batch/_measure_operation_breakdown.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=cg-bench
+#SBATCH --ntasks=8
+#SBATCH --gres=gpu:8
+#SBATCH --partition=dgx2q
+#SBATCH --time=06:00:00
+#SBATCH --output=sbatch_output_%j.log
+
+cd ~/multi-perks/CG
+
+. ./scripts/modules.sh > /dev/null
+
+echo "--- RUNNING ---"
+date
+
+python3 ./scripts/measure_operation_breakdown.py
+
+echo ""
+
+echo "--- DONE ---"
+date
