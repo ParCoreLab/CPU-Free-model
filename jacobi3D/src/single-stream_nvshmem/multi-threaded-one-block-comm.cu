3fafdf2aac105eda65c66fc0924498724088126b
dogansagbili <dogansagbili@hotmail.com>
2022-10-31 20:11:05 +0300

    a

--- jacobi3D/src/single-stream_nvshmem/multi-threaded-one-block-comm.cu
+++ jacobi3D/src/single-stream_nvshmem/multi-threaded-one-block-comm.cu
@@ -35,7 +35,11 @@ namespace SSMultiThreadedOneBlockCommNvshmem
         {
             if (blockIdx.x == gridDim.x - 1)
             {
-                nvshmem_signal_wait_until(is_done_computing_flags, NVSHMEM_CMP_EQ, iter);
+                if (cta.thread_rank()==0)
+                {
+                    nvshmem_signal_wait_until(is_done_computing_flags, NVSHMEM_CMP_EQ, iter);
+                }
+                cg::sync(cta);
 
                 for (int iy = (threadIdx.z * blockDim.y + threadIdx.y + 1); iy < (ny - 1); iy += blockDim.y * blockDim.z)
                 {
@@ -55,7 +59,12 @@ namespace SSMultiThreadedOneBlockCommNvshmem
                     (real *)&halo_buffer_bottom[next_iter_mod * ny * nx], (real *)&a_new[iz_start * ny * nx],
                     ny * nx * sizeof(real), is_done_computing_flags + 1, iter + 1, NVSHMEM_SIGNAL_SET, top);
 
-                nvshmem_signal_wait_until(is_done_computing_flags + 1, NVSHMEM_CMP_EQ, iter);
+                if (cta.thread_rank()==0)
+                {
+                    nvshmem_signal_wait_until(is_done_computing_flags + 1, NVSHMEM_CMP_EQ, iter);
+                }
+                cg::sync(cta);
+                
 
                 for (int iy = (threadIdx.z * blockDim.y + threadIdx.y + 1); iy < (ny - 1); iy += blockDim.y * blockDim.z)
                 {
