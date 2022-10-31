3ca15ec660f9e0afcf3f65be1dc1eb22b8df112b
dogansagbili <dogansagbili@hotmail.com>
2022-10-31 20:48:44 +0300

    a

--- jacobi3D/src/single-stream_nvshmem/multi-threaded-one-block-comm.cu
+++ jacobi3D/src/single-stream_nvshmem/multi-threaded-one-block-comm.cu
@@ -37,7 +37,7 @@ namespace SSMultiThreadedOneBlockCommNvshmem
             {
                 if (cta.thread_rank() == 0)
                 {
-                    nvshmem_signal_wait_until(is_done_computing_flags + cur_iter_mod, NVSHMEM_CMP_EQ, iter);
+                    nvshmem_signal_wait_until(is_done_computing_flags, NVSHMEM_CMP_EQ, iter);
                 }
                 cg::sync(cta);
                 for (int iy = (threadIdx.z * blockDim.y + threadIdx.y + 1); iy < (ny - 1); iy += blockDim.y * blockDim.z)
@@ -56,11 +56,11 @@ namespace SSMultiThreadedOneBlockCommNvshmem
 
                 nvshmemx_putmem_signal_block(
                     (real *)&halo_buffer_bottom[next_iter_mod * ny * nx], (real *)&a_new[iz_start * ny * nx],
-                    ny * nx * sizeof(real), &is_done_computing_flags[next_iter_mod * 2 + 1], iter + 1, NVSHMEM_SIGNAL_SET, top);
+                    ny * nx * sizeof(real), &is_done_computing_flags[1], iter + 1, NVSHMEM_SIGNAL_SET, top);
 
                 if (cta.thread_rank() == 0)
                 {
-                    nvshmem_signal_wait_until(is_done_computing_flags + cur_iter_mod + 1, NVSHMEM_CMP_EQ, iter);
+                    nvshmem_signal_wait_until(is_done_computing_flags + 1, NVSHMEM_CMP_EQ, iter);
                 }
                 cg::sync(cta);
                 for (int iy = (threadIdx.z * blockDim.y + threadIdx.y + 1); iy < (ny - 1); iy += blockDim.y * blockDim.z)
@@ -78,7 +78,7 @@ namespace SSMultiThreadedOneBlockCommNvshmem
                 }
                 nvshmemx_putmem_signal_block(
                     (real *)&halo_buffer_top[next_iter_mod * ny * nx], (real *)&a_new[(iz_end - 1) * ny * nx],
-                    ny * nx * sizeof(real), &is_done_computing_flags[next_iter_mod * 2], iter + 1, NVSHMEM_SIGNAL_SET, bottom);
+                    ny * nx * sizeof(real), is_done_computing_flags, iter + 1, NVSHMEM_SIGNAL_SET, bottom);
             }
             else
             {
