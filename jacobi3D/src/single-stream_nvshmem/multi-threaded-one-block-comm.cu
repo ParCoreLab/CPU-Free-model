bb6a2e4b26ef32ca47142dafc2bc27631a84b83e
dogansagbili <dogansagbili@hotmail.com>
2022-11-01 14:33:35 +0300

    testing weak

--- jacobi3D/src/single-stream_nvshmem/multi-threaded-one-block-comm.cu
+++ jacobi3D/src/single-stream_nvshmem/multi-threaded-one-block-comm.cu
@@ -113,7 +113,7 @@ int SSMultiThreadedOneBlockCommNvshmem::init(int argc, char *argv[])
     const int iter_max = get_argval<int>(argv, argv + argc, "-niter", 1000);
     const int nx = get_argval<int>(argv, argv + argc, "-nx", 512);
     const int ny = get_argval<int>(argv, argv + argc, "-ny", 512);
-    const int nz = get_argval<int>(argv, argv + argc, "-nz", 512);
+    const int nz = get_argval<int>(argv, argv + argc, "-nz", 4096);
     const bool compare_to_single_gpu = get_arg(argv, argv + argc, "-compare");
 
     real *a;
