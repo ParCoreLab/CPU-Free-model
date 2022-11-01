9852a01562b55dcbd05df626d81e7c768496b28a
dogansagbili <dogansagbili@hotmail.com>
2022-11-02 01:17:28 +0300

    revert

--- jacobi3D/scripts/weak_scale_bench.sh
+++ jacobi3D/scripts/weak_scale_bench.sh
@@ -90,7 +90,7 @@ for version_name in "${!version_name_to_idx_map[@]}"; do
 
         for (( i=1; i <= ${NUM_RUNS}; i++ )); do
             execution_time=$(${BIN} -v ${version_idx} -nx ${NX} -ny ${NY} -nz  ${NZ} -niter ${NUM_ITER})
-            echo "${execution_time}"
+            echo "${execution_time} on run ${i}"
         done
 
         printf "\n"
