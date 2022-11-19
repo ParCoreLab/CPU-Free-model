114b93fb31b2dcf25f2799e1be4ee97a5eb9a58c
dogansagbili <dogansagbili@hotmail.com>
2022-11-19 23:25:49 +0300

    warnings removed

--- jacobi3D/scripts/modules_truba.sh
+++ jacobi3D/scripts/modules_truba.sh
@@ -7,4 +7,5 @@ export UCX_HOME=/truba/home/dsagbili/spack/opt/spack/linux-rhel8-zen/gcc-8.5.0/u
 export NVSHMEM_HOME=/truba/home/dsagbili/spack/opt/spack/linux-rhel8-zen/gcc-8.5.0/nvshmem-2.7.0-6-pdl77w7adu5dm334pezvemvt5tjxsowg
 export MPI_HOME=/truba/home/dsagbili/spack/opt/spack/linux-rhel8-zen/gcc-8.5.0/openmpi-4.1.4-ycvxffyzzonogvqycd4gpp7aholtkss5
 export CUDA_HOME=/truba/home/dsagbili/spack/opt/spack/linux-rhel8-zen/gcc-8.5.0/cuda-11.8.0-37xn6z7age2zvgrmug5jad7l34sizzkp
+cd /truba/home/dsagbili/multi-perks/jacobi3D
 make -B
