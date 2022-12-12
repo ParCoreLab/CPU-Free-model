5fd0968b566e6ddb9f196e9a36c4d185a6b058ef
Javid Baydamirli <jbaydamirli21@ku.edu.tr>
2022-12-12 17:52:04 +0300

    Added missing PERKS header

---
+++ jacobi3D/include/PERKS/multi-stream-perks.cuh
@@ -0,0 +1,10 @@
+#ifndef INC_3D_STENCIL_MULTI_THREADED_PERKS_H
+#define INC_3D_STENCIL_MULTI_THREADED_PERKS_H
+
+#include "../common.h"
+
+namespace MultiStreamPERKS {
+int init(int argc, char** argv);
+}
+
+#endif  // INC_3D_STENCIL_MULTI_THREADED_PERKS_H
