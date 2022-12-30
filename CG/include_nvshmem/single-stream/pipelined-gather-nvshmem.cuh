3f05b7d4edf071850a85ebbe5e586fa4e0c21b81
Ismayil Ismayilov <iismayilov4574@gmail.com>
2022-12-30 18:06:19 +0300

    Implement gathering the vector to do SpMV computation locally
    Won't actually be running this because performance is horrible. Will keep it around just in case.

---
+++ CG/include_nvshmem/single-stream/pipelined-gather-nvshmem.cuh
@@ -0,0 +1,10 @@
+#ifndef INC_CG_SINGLE_STREAM_PIPELINED_GATHER_NVSHMEM_CUH
+#define INC_CG_SINGLE_STREAM_PIPELINED_GATHER_NVSHMEM_CUH
+
+#include "../common.h"
+
+namespace SingleStreamPipelinedGatherNVSHMEM {
+int init(int argc, char **argv);
+}
+
+#endif  // INC_CG_SINGLE_STREAM_PIPELINED_GATHER_NVSHMEM_CUH
