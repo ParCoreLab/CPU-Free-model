1cbdca819da5ffaab0c1b5d5b7e2fbb80f7fded8
Ismayil Ismayilov <iismayilov4574@gmail.com>
2023-01-10 16:02:01 +0300

    Move redundant MPI initialization to main.cu

--- CG/src_nvshmem/baseline/discrete-standard-nvshmem.cu
+++ CG/src_nvshmem/baseline/discrete-standard-nvshmem.cu
@@ -102,41 +102,13 @@ __global__ void resetLocalDotProduct(double *dot_result) {
 
 }  // namespace BaselineDiscreteStandardNVSHMEM
 
-int BaselineDiscreteStandardNVSHMEM::init(int argc, char *argv[]) {
-    const int iter_max = get_argval<int>(argv, argv + argc, "-niter", 10000);
-    std::string matrix_path_str = get_argval<std::string>(argv, argv + argc, "-matrix_path", "");
-    const bool compare_to_single_gpu = get_arg(argv, argv + argc, "-compare-single-gpu");
-    const bool compare_to_cpu = get_arg(argv, argv + argc, "-compare-cpu");
-
-    char *matrix_path_char = const_cast<char *>(matrix_path_str.c_str());
-    bool generate_random_tridiag_matrix = matrix_path_str.empty();
-
-    int num_devices = 0;
-    double single_gpu_runtime;
-
-    CUDA_RT_CALL(cudaGetDeviceCount(&num_devices));
-
-    int num_rows = 0;
-    int num_cols = 0;
-    int nnz = 0;
-    bool matrix_is_zero_indexed;
-
-    int *host_csrRowIndices = NULL;
-    int *host_csrColIndices = NULL;
-    real *host_csrVal = NULL;
-
-    real *x_ref_single_gpu = NULL;
-    real *x_final_result = NULL;
-
-    real *s_cpu = NULL;
-    real *r_cpu = NULL;
-    real *p_cpu = NULL;
-    real *x_ref_cpu = NULL;
-
-    int *device_csrRowIndices = NULL;
-    int *device_csrColIndices = NULL;
-    real *device_csrVal = NULL;
-
+int BaselineDiscreteStandardNVSHMEM::init(int *device_csrRowIndices, int *device_csrColIndices,
+                                          real *device_csrVal, const int num_rows, const int nnz,
+                                          bool matrix_is_zero_indexed, const int num_devices,
+                                          const int iter_max, real *x_final_result,
+                                          const double single_gpu_runtime,
+                                          bool compare_to_single_gpu, bool compare_to_cpu,
+                                          real *x_ref_single_gpu, real *x_ref_cpu) {
     real *device_x;
     real *device_r;
     real *device_p;
@@ -157,91 +129,17 @@ int BaselineDiscreteStandardNVSHMEM::init(int argc, char *argv[]) {
     real real_positive_one = 1.0;
     real real_negative_one = -1.0;
 
-    int rank = 0, size = 1;
-    MPI_CALL(MPI_Init(&argc, &argv));
-    MPI_CALL(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
-    MPI_CALL(MPI_Comm_size(MPI_COMM_WORLD, &size));
-
-    int local_rank = -1;
-    int local_size = 1;
-    {
-        MPI_Comm local_comm;
-        MPI_CALL(MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, rank, MPI_INFO_NULL,
-                                     &local_comm));
-
-        MPI_CALL(MPI_Comm_rank(local_comm, &local_rank));
-        MPI_CALL(MPI_Comm_size(local_comm, &local_size));
-
-        MPI_CALL(MPI_Comm_free(&local_comm));
-    }
-
-    if (1 < num_devices && num_devices < local_size) {
-        fprintf(stderr,
-                "ERROR Number of visible devices (%d) is less than number of ranks on the "
-                "node (%d)!\n",
-                num_devices, local_size);
-        MPI_CALL(MPI_Finalize());
-        return 1;
-    }
-    if (1 == num_devices) {
-        // Only 1 device visible, assuming GPU affinity is handled via CUDA_VISIBLE_DEVICES
-        CUDA_RT_CALL(cudaSetDevice(0));
-    } else {
-        CUDA_RT_CALL(cudaSetDevice(local_rank));
-    }
-
-    CUDA_RT_CALL(cudaFree(0));
-
     MPI_Comm mpi_comm;
     nvshmemx_init_attr_t attr;
 
     mpi_comm = MPI_COMM_WORLD;
     attr.mpi_comm = &mpi_comm;
 
-    if (generate_random_tridiag_matrix) {
-        num_rows = 10485760 * 2;
-        num_cols = num_rows;
-
-        nnz = (num_rows - 2) * 3 + 4;
-
-        host_csrRowIndices = (int *)malloc(sizeof(int) * (num_rows + 1));
-        host_csrColIndices = (int *)malloc(sizeof(int) * nnz);
-        host_csrVal = (real *)malloc(sizeof(real) * nnz);
-
-        /* Generate a random tridiagonal symmetric matrix in CSR format */
-        genTridiag(host_csrRowIndices, host_csrColIndices, host_csrVal, num_rows, nnz);
-    } else {
-        if (loadMMSparseMatrix<real>(matrix_path_char, 'd', true, &num_rows, &num_cols, &nnz,
-                                     &host_csrVal, &host_csrRowIndices, &host_csrColIndices,
-                                     true)) {
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    // Check if matrix is 0 or 1 indexed
-    int index_base = host_csrRowIndices[0];
-
-    if (index_base == 1) {
-        matrix_is_zero_indexed = false;
-    } else if (index_base == 0) {
-        matrix_is_zero_indexed = true;
-    }
-
-    CUDA_RT_CALL(cudaMalloc((void **)&device_csrRowIndices, sizeof(int) * (num_rows + 1)));
-    CUDA_RT_CALL(cudaMalloc((void **)&device_csrColIndices, sizeof(int) * nnz));
-    CUDA_RT_CALL(cudaMalloc((void **)&device_csrVal, sizeof(real) * nnz));
-
-    CUDA_RT_CALL(cudaMemcpy(device_csrRowIndices, host_csrRowIndices, sizeof(int) * (num_rows + 1),
-                            cudaMemcpyHostToDevice));
-    CUDA_RT_CALL(cudaMemcpy(device_csrColIndices, host_csrColIndices, sizeof(int) * nnz,
-                            cudaMemcpyHostToDevice));
-    CUDA_RT_CALL(
-        cudaMemcpy(device_csrVal, host_csrVal, sizeof(real) * nnz, cudaMemcpyHostToDevice));
-
     // Set symmetric heap size for nvshmem based on problem size
     // Its default value in nvshmem is 1 GB which is not sufficient
     // for large mesh sizes
-    long long unsigned int mesh_size_per_rank = num_rows / size + (num_rows % size != 0);
+    long long unsigned int mesh_size_per_rank =
+        num_rows / num_devices + (num_rows % num_devices != 0);
 
     long long unsigned int required_symmetric_heap_size =
         5 * mesh_size_per_rank * sizeof(real) * 1.1;
@@ -268,6 +166,7 @@ int BaselineDiscreteStandardNVSHMEM::init(int argc, char *argv[]) {
 
         setenv("NVSHMEM_SYMMETRIC_SIZE", symmetric_heap_size_str, 1);
     }
+
     nvshmemx_init_attr(NVSHMEMX_INIT_WITH_MPI_COMM, &attr);
 
     int npes = nvshmem_n_pes();
@@ -308,35 +207,6 @@ int BaselineDiscreteStandardNVSHMEM::init(int argc, char *argv[]) {
     int row_end_global_idx = (mype + 1) * chunk_size;  // My end index in the global array
 
     row_end_global_idx = std::min(row_end_global_idx, num_rows);
-
-    if (compare_to_single_gpu) {
-        CUDA_RT_CALL(cudaMallocHost(&x_ref_single_gpu, num_rows * sizeof(real)));
-
-        single_gpu_runtime = SingleGPUDiscreteStandard::run_single_gpu(
-            iter_max, device_csrRowIndices, device_csrColIndices, device_csrVal, x_ref_single_gpu,
-            num_rows, nnz, matrix_is_zero_indexed);
-
-        // single_gpu_runtime = SingleGPUDiscretePipelined::run_single_gpu(
-        //     iter_max, device_csrRowIndices, device_csrColIndices, device_csrVal,
-        //     x_ref_single_gpu, num_rows, nnz, matrix_is_zero_indexed);
-    }
-
-    if (compare_to_cpu) {
-        s_cpu = (real *)malloc(sizeof(real) * num_rows);
-        r_cpu = (real *)malloc(sizeof(real) * num_rows);
-        p_cpu = (real *)malloc(sizeof(real) * num_rows);
-
-        CUDA_RT_CALL(cudaMallocHost(&x_ref_cpu, num_rows * sizeof(real)));
-
-        for (int i = 0; i < num_rows; i++) {
-            r_cpu[i] = 1.0;
-            s_cpu[i] = 0.0;
-            x_ref_cpu[i] = 0.0;
-        }
-
-        CPU::cpuConjugateGrad(iter_max, host_csrRowIndices, host_csrColIndices, host_csrVal,
-                              x_ref_cpu, s_cpu, p_cpu, r_cpu, nnz, num_rows, tol);
-    }
 
     CUDA_RT_CALL(cudaDeviceSynchronize());
     nvshmem_barrier_all();
@@ -462,8 +332,6 @@ int BaselineDiscreteStandardNVSHMEM::init(int argc, char *argv[]) {
     double stop = MPI_Wtime();
 
     if (compare_to_single_gpu || compare_to_cpu) {
-        CUDA_RT_CALL(cudaMallocHost(&x_final_result, num_rows * sizeof(real)));
-
         // Need to do this when when num_rows % npes != 0
         int num_elems_to_copy = row_end_global_idx - row_start_global_idx;
 
@@ -496,30 +364,7 @@ int BaselineDiscreteStandardNVSHMEM::init(int argc, char *argv[]) {
 
     CUDA_RT_CALL(cudaStreamDestroy(mainStream));
 
-    CUDA_RT_CALL(cudaFree(device_csrRowIndices));
-    CUDA_RT_CALL(cudaFree(device_csrColIndices));
-    CUDA_RT_CALL(cudaFree(device_csrVal));
-
-    free(host_csrRowIndices);
-    free(host_csrColIndices);
-    free(host_csrVal);
-
-    if (compare_to_single_gpu || compare_to_cpu) {
-        cudaFreeHost(x_final_result);
-
-        if (compare_to_single_gpu) {
-            cudaFreeHost(x_ref_single_gpu);
-        }
-
-        if (compare_to_cpu) {
-            cudaFreeHost(x_ref_cpu);
-
-            // Free CPU arrays here
-        }
-    }
-
     nvshmem_finalize();
-    MPI_CALL(MPI_Finalize());
 
     return 0;
 }
--- CG/src_nvshmem/main.cu
+++ CG/src_nvshmem/main.cu
@@ -11,6 +11,8 @@
 
 #include "../include_nvshmem/common.h"
 
+#include <mpi.h>
+
 using std::make_pair;
 
 int main(int argc, char *argv[]) {
@@ -29,6 +31,141 @@ int main(int argc, char *argv[]) {
 
     const int selection = get_argval<int>(argv, argv + argc, "-v", 0);
     const bool silent = get_arg(argv, argv + argc, "-s");
+    const int iter_max = get_argval<int>(argv, argv + argc, "-niter", 10000);
+    std::string matrix_path_str = get_argval<std::string>(argv, argv + argc, "-matrix_path", "");
+    const bool compare_to_single_gpu = get_arg(argv, argv + argc, "-compare-single-gpu");
+    const bool compare_to_cpu = get_arg(argv, argv + argc, "-compare-cpu");
+
+    char *matrix_path_char = const_cast<char *>(matrix_path_str.c_str());
+    bool generate_random_tridiag_matrix = matrix_path_str.empty();
+
+    int num_devices = 0;
+    double single_gpu_runtime;
+
+    CUDA_RT_CALL(cudaGetDeviceCount(&num_devices));
+
+    int num_rows = 0;
+    int num_cols = 0;
+    int nnz = 0;
+    bool matrix_is_zero_indexed;
+
+    int *host_csrRowIndices = NULL;
+    int *host_csrColIndices = NULL;
+    real *host_csrVal = NULL;
+
+    real *x_ref_single_gpu = NULL;
+    real *x_final_result = NULL;
+
+    real *s_cpu = NULL;
+    real *r_cpu = NULL;
+    real *p_cpu = NULL;
+    real *x_ref_cpu = NULL;
+
+    int *device_csrRowIndices = NULL;
+    int *device_csrColIndices = NULL;
+    real *device_csrVal = NULL;
+
+    int rank = 0, size = 1;
+    MPI_CALL(MPI_Init(&argc, &argv));
+    MPI_CALL(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+    MPI_CALL(MPI_Comm_size(MPI_COMM_WORLD, &size));
+
+    int local_rank = -1;
+    int local_size = 1;
+    {
+        MPI_Comm local_comm;
+        MPI_CALL(MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, rank, MPI_INFO_NULL,
+                                     &local_comm));
+
+        MPI_CALL(MPI_Comm_rank(local_comm, &local_rank));
+        MPI_CALL(MPI_Comm_size(local_comm, &local_size));
+
+        MPI_CALL(MPI_Comm_free(&local_comm));
+    }
+
+    if (1 < num_devices && num_devices < local_size) {
+        fprintf(stderr,
+                "ERROR Number of visible devices (%d) is less than number of ranks on the "
+                "node (%d)!\n",
+                num_devices, local_size);
+        MPI_CALL(MPI_Finalize());
+        return 1;
+    }
+    if (1 == num_devices) {
+        // Only 1 device visible, assuming GPU affinity is handled via CUDA_VISIBLE_DEVICES
+        CUDA_RT_CALL(cudaSetDevice(0));
+    } else {
+        CUDA_RT_CALL(cudaSetDevice(local_rank));
+    }
+
+    CUDA_RT_CALL(cudaFree(0));
+
+    if (generate_random_tridiag_matrix) {
+        num_rows = 10485760 * 2;
+        num_cols = num_rows;
+
+        nnz = (num_rows - 2) * 3 + 4;
+
+        host_csrRowIndices = (int *)malloc(sizeof(int) * (num_rows + 1));
+        host_csrColIndices = (int *)malloc(sizeof(int) * nnz);
+        host_csrVal = (real *)malloc(sizeof(real) * nnz);
+
+        /* Generate a random tridiagonal symmetric matrix in CSR format */
+        genTridiag(host_csrRowIndices, host_csrColIndices, host_csrVal, num_rows, nnz);
+    } else {
+        if (loadMMSparseMatrix<real>(matrix_path_char, 'd', true, &num_rows, &num_cols, &nnz,
+                                     &host_csrVal, &host_csrRowIndices, &host_csrColIndices,
+                                     true)) {
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    CUDA_RT_CALL(cudaMallocHost(&x_final_result, num_rows * sizeof(real)));
+
+    // Check if matrix is 0 or 1 indexed
+    int index_base = host_csrRowIndices[0];
+
+    if (index_base == 1) {
+        matrix_is_zero_indexed = false;
+    } else if (index_base == 0) {
+        matrix_is_zero_indexed = true;
+    }
+
+    CUDA_RT_CALL(cudaMalloc((void **)&device_csrRowIndices, sizeof(int) * (num_rows + 1)));
+    CUDA_RT_CALL(cudaMalloc((void **)&device_csrColIndices, sizeof(int) * nnz));
+    CUDA_RT_CALL(cudaMalloc((void **)&device_csrVal, sizeof(real) * nnz));
+
+    CUDA_RT_CALL(cudaMemcpy(device_csrRowIndices, host_csrRowIndices, sizeof(int) * (num_rows + 1),
+                            cudaMemcpyHostToDevice));
+    CUDA_RT_CALL(cudaMemcpy(device_csrColIndices, host_csrColIndices, sizeof(int) * nnz,
+                            cudaMemcpyHostToDevice));
+    CUDA_RT_CALL(
+        cudaMemcpy(device_csrVal, host_csrVal, sizeof(real) * nnz, cudaMemcpyHostToDevice));
+
+    if (compare_to_single_gpu) {
+        CUDA_RT_CALL(cudaMallocHost(&x_ref_single_gpu, num_rows * sizeof(real)));
+
+        single_gpu_runtime = SingleGPUDiscreteStandard::run_single_gpu(
+            iter_max, device_csrRowIndices, device_csrColIndices, device_csrVal, x_ref_single_gpu,
+            num_rows, nnz, matrix_is_zero_indexed);
+    }
+
+    if (compare_to_cpu) {
+        s_cpu = (real *)malloc(sizeof(real) * num_rows);
+        r_cpu = (real *)malloc(sizeof(real) * num_rows);
+        p_cpu = (real *)malloc(sizeof(real) * num_rows);
+
+        CUDA_RT_CALL(cudaMallocHost(&x_ref_cpu, num_rows * sizeof(real)));
+
+        for (int i = 0; i < num_rows; i++) {
+            r_cpu[i] = 1.0;
+            s_cpu[i] = 0.0;
+            x_ref_cpu[i] = 0.0;
+        }
+
+        CPU::cpuConjugateGrad(iter_max, host_csrRowIndices, host_csrColIndices, host_csrVal,
+                              x_ref_cpu, s_cpu, p_cpu, r_cpu, nnz, num_rows, tol);
+    }
 
     auto &selected = versions[selection];
 
@@ -44,5 +181,30 @@ int main(int argc, char *argv[]) {
         std::cout << "Running " << selected.first << "\n" << std::endl;
     }
 
-    return selected.second(argc, argv);
+    selected.second(device_csrRowIndices, device_csrColIndices, device_csrVal, num_rows, nnz,
+                    matrix_is_zero_indexed, num_devices, iter_max, x_final_result,
+                    single_gpu_runtime, compare_to_single_gpu, compare_to_cpu, x_ref_single_gpu,
+                    x_ref_cpu);
+
+    CUDA_RT_CALL(cudaFree(device_csrRowIndices));
+    CUDA_RT_CALL(cudaFree(device_csrColIndices));
+    CUDA_RT_CALL(cudaFree(device_csrVal));
+
+    free(host_csrRowIndices);
+    free(host_csrColIndices);
+    free(host_csrVal);
+
+    if (compare_to_single_gpu || compare_to_cpu) {
+        cudaFreeHost(x_final_result);
+
+        if (compare_to_single_gpu) {
+            cudaFreeHost(x_ref_single_gpu);
+        }
+
+        if (compare_to_cpu) {
+            cudaFreeHost(x_ref_cpu);
+        }
+    }
+
+    MPI_CALL(MPI_Finalize());
 }
