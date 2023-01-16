b341ce9b0bd39306da1fe0034232f3887cfccb84
Javid Baydamirli <jbaydamirli21@ku.edu.tr>
2023-01-16 20:39:17 +0300

    Added base implementation boilerplate for PERKS implementation

---
+++ jacobi2D/include/PERKS/multi-stream-perks.cuh
@@ -0,0 +1,10 @@
+#ifndef INC_2D_STENCIL_MULTI_STREAM_PERKS_CUH
+#define INC_2D_STENCIL_MULTI_STREAM_PERKS_CUH
+
+#include "../common.h"
+
+namespace MultiStreamPERKS {
+int init(int argc, char** argv);
+}
+
+#endif  // INC_2D_STENCIL_MULTI_STREAM_PERKS_CUH
