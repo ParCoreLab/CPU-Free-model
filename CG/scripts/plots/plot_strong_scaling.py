c98b817c4bf0e50c372769b44dc0c3970f356c75
Ismayil Ismayilov <iismayilov4574@gmail.com>
2023-01-26 14:52:00 +0300

    Working on strong scaling plots

--- CG/scripts/plots/plot_strong_scaling.py
+++ CG/scripts/plots/plot_strong_scaling.py
@@ -12,28 +12,43 @@ import pandas as pd
 import numpy as np
 
 MATRIX_NAMES = [
-    'tridiagonal',
-    'ecology2',
+    # 'tridiagonal',
+    # 'ecology2',
     #   'shallow_water2', Too little non-zeros
     #   'Trefethen_2000', Too little non-zeros
-    'hood',
-    'bmwcra_1',
-    'consph',
+    # 'hood',
+    # 'bmwcra_1',
+    # 'consph',
     # 'thermomech_dM',
     # 'tmt_sym',
-    # 'crankseg_1',
-    # 'crankseg_2',
+    'crankseg_1',
+    'crankseg_2',
     # 'Queen_4147',
     # 'Bump_2911',
-    # 'G3_circuit',
-    # 'StocF-1465',
-    # 'Flan_1565',
+    'G3_circuit',
+    'StocF-1465',
+    'Flan_1565',
     # 'audikw_1',
     # 'Serena',
     # 'Geo_1438',
     # 'Hook_1498',
     #   'bone010', Multi-part matrix, don't handle those for now
     'ldoor'
+]
+
+LARGE_MATRICES_TO_PLOT = [
+    'Queen_4147',
+    'Bump_2911',
+    'Flan_1565',
+    'Hook_1498',
+    'Flan_1565',
+    'ldoor'
+]
+
+MEDIUM_SMALL_MATRICES_TO_PLOT = [
+    'ecology2',
+    'G3_circuit',
+    'StocF-1465',
 ]
 
 VERSIONS_TO_KEEP = [
@@ -43,6 +58,8 @@ VERSIONS_TO_KEEP = [
     '(Ours) Persistent Pipelined'
 ]
 
+MATRICES_TO_PLOT = LARGE_MATRICES_TO_PLOT + MEDIUM_SMALL_MATRICES_TO_PLOT
+
 MODULE_DIR = get_module_dir('Strong Scaling')
 
 dir_path = dirname(realpath(__file__))
@@ -50,8 +67,8 @@ dir_path = dirname(realpath(__file__))
 plt.style.use(
     'https://github.com/dhaitz/matplotlib-stylesheets/raw/master/pitayasmoothie-light.mplstyle')
 
-plots = len(MATRIX_NAMES)
-fig, axes = plt.subplots(math.ceil(plots / 3), 3)
+plots = len(MATRICES_TO_PLOT)
+fig, axes = plt.subplots(math.ceil(plots / 3), 3, layout='constrained')
 fig.set_size_inches(18, 4 * math.ceil(plots / 3))
 
 files = get_files()
@@ -62,20 +79,20 @@ for file in files:
     data = pd.read_csv(file, index_col='Version')
     data = data.sort_index()
 
-    for ax, matrix_name in zip(axes.flatten(), MATRIX_NAMES):
+    for ax, matrix_name in zip(axes.flatten(), MATRICES_TO_PLOT):
         per_matrix_data = data.loc[data['Matrix'] == matrix_name]
         per_matrix_data = per_matrix_data.drop(columns=['Matrix'])
         per_matrix_data = per_matrix_data.T
         per_matrix_data = pd.DataFrame(
             per_matrix_data, columns=VERSIONS_TO_KEEP)
 
-        tmp_axes = per_matrix_data.plot(ax=ax)
-        tmp_axes.set_title(matrix_name)
+        tmp_axes = per_matrix_data.plot(ax=ax, linewidth=2.0)
+        tmp_axes.set_title(matrix_name, weight='bold')
 
         markers_cycle = cycle(markers)
 
         # Skip first few markers since they are underwhelming
-        offset_markers_cycle = islice(markers_cycle, 11, None)
+        offset_markers_cycle = islice(markers_cycle, 10, None)
 
         for line in tmp_axes.get_lines():
             line.set_marker(next(offset_markers_cycle))
@@ -92,13 +109,14 @@ for file in files:
 
     handles, labels = tmp_axes.get_legend_handles_labels()
 
-    legend = fig.legend(handles, labels, loc='upper center',
-                        ncol=2)
+    handles, labels = ax.get_legend_handles_labels()
+    axes.flatten()[2].legend(loc='best',
+                             fancybox=True, prop={'weight': 'bold', 'size': 'large'})
 
     title = Path(files[0].name).stem
 
-    plt.tight_layout()
-    plt.savefig(MODULE_DIR / title, bbox_extra_artists=(legend,),
-                bbox_inches='tight')
+    y_label = fig.supylabel('Time (s)', weight='bold')
+
+    plt.savefig(MODULE_DIR / title)
 
     plt.show()
