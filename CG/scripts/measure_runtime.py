9a922049d3a40fd763034afe3c4b50b6bf697225
Ismayil Ismayilov <iismayilov4574@gmail.com>
2023-01-29 20:32:04 +0300

    Save runtime results as csv

--- CG/scripts/measure_runtime.py
+++ CG/scripts/measure_runtime.py
@@ -32,9 +32,9 @@ GPU_MODEL = None
 USING_NVSHMEM = True
 
 VERSION_NAME_TO_IDX_MAP_NVSHMEM = {
-    'Baseline Discrete Standard NVSHMEM': 0,
-    'Baseline Discrete Pipelined NVSHMEM': 1,
-    'Baseline Persistent Standard NVSHMEM': 2,
+    '(Baseline) Discrete Standard NVSHMEM': 0,
+    '(Baseline) Discrete Pipelined NVSHMEM': 1,
+    '(Ours) Persistent Standard NVSHMEM': 2,
     '(Ours) Persistent Pipelined NVSHMEM': 3,
     '(Ours) Persistent Pipelined Multi-Overlap NVSHMEM': 4,
     '(Ours) Persistent Standard Saxpy Overlap NVSHMEM': 5
@@ -44,7 +44,7 @@ VERSION_NAME_TO_IDX_MAP = VERSION_NAME_TO_IDX_MAP_NVSHMEM.copy()
 VERSION_INDICES_TO_RUN = list(VERSION_NAME_TO_IDX_MAP_NVSHMEM.values())
 
 MATRIX_NAMES = [
-    '(generated)_tridiagonal',
+    'tridiagonal',
     'ecology2',
     #   'shallow_water2', Too little non-zeros
     #   'Trefethen_2000', Too little non-zeros
@@ -73,23 +73,18 @@ VERSION_LABELS = VERSION_NAME_TO_IDX_MAP.keys()
 GPU_COLUMN_NAMES = None
 
 
-def get_perf_data_string(version_to_result_map, column_labels):
-    ephemereal_csv_file = io.StringIO('')
+def write_to_csv(matrix_to_version_to_result_map, column_labels, output_csv_file):
+    padded_column_labels = ['Matrix', 'Version'] + column_labels
 
-    csv_writer = csv.writer(ephemereal_csv_file, delimiter=',')
+    with open(output_csv_file, 'w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',')
+        csv_writer.writerow([f'Execution time on {MAX_NUM_GPUS}{GPU_MODEL}'])
+        csv_writer.writerow(padded_column_labels)
 
-    # Add empty string column to get table-like output
-    padded_column_labels = [''] + column_labels
-
-    csv_writer.writerow(padded_column_labels)
-
-    for row_label, runtimes in version_to_result_map.items():
-        final_row = [row_label] + runtimes
-        csv_writer.writerow(final_row)
-
-    perf_data_string = ephemereal_csv_file.getvalue()
-
-    return perf_data_string
+        for matrix_name, version_to_result_map in matrix_to_version_to_result_map.items():
+            for version_name, runtimes in version_to_result_map.items():
+                final_row = [matrix_name, version_name] + runtimes
+                csv_writer.writerow(final_row)
 
 
 def measure_runtime(save_result_to_path, executable_dir):
@@ -102,7 +97,7 @@ def measure_runtime(save_result_to_path, executable_dir):
     for matrix_name in MATRIX_NAMES:
         matrix_path = MATRICES_FOLDER_PATH + '/' + matrix_name + '.mtx'
 
-        if 'generated' in matrix_name:
+        if 'tridiagonal' in matrix_name:
             matrix_path = None
 
         version_to_result_map = defaultdict(list)
@@ -168,27 +163,8 @@ def measure_runtime(save_result_to_path, executable_dir):
 
         version_to_matrix_to_result_map[version_name] = matrix_to_result_map
 
-    with open(save_result_to_path, 'a') as output_file:
-        output_file.write('Results per matrix; rows are versions')
-        output_file.write('\n\n')
-
-        for matrix_name, version_to_result_map in matrix_to_version_to_result_map.items():
-            output_file.write(f'Results for matrix {matrix_name} =>')
-            output_file.write('\n')
-            output_file.write(get_perf_data_string(
-                version_to_result_map, GPU_COLUMN_NAMES))
-            output_file.write('\n\n')
-
-        output_file.write('\n')
-        output_file.write('Results per version; rows are matrices')
-        output_file.write('\n\n')
-
-        for version_name, matrix_to_result_map in version_to_matrix_to_result_map.items():
-            output_file.write(f'Results for version {version_name} =>')
-            output_file.write('\n')
-            output_file.write(get_perf_data_string(
-                matrix_to_result_map, GPU_COLUMN_NAMES))
-            output_file.write('\n\n')
+    write_to_csv(matrix_to_version_to_result_map,
+                 GPU_COLUMN_NAMES, save_result_to_path)
 
 
 if __name__ == "__main__":
@@ -229,6 +205,7 @@ if __name__ == "__main__":
                                for gpu_num in gpu_nums_to_run]
 
             GPU_NUMS_TO_RUN = gpu_nums_to_run[:]
+            MAX_NUM_GPUS = max(GPU_NUMS_TO_RUN)
 
         if sys.argv[arg_idx] == '--versions_to_run':
             arg_idx += 1
@@ -263,7 +240,7 @@ if __name__ == "__main__":
 
     if FILENAME == None:
         FILENAME = BASE_FILENAME + '-' + datetime.now().strftime('%d-%m-%Y_%H-%M-%S') + \
-            f'-{GPU_MODEL}' + '.txt'
+            f'-{GPU_MODEL}' + '.csv'
 
     VERSION_LABELS = VERSION_NAME_TO_IDX_MAP.keys()
 
