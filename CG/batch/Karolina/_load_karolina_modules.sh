afe338efa10650176e32aeb2a1ee4a939981b9f6
Ismayil Ismayilov <iismayilov4574@gmail.com>
2023-08-12 23:59:54 +0300

    Load PETSc on Karolina

--- CG/batch/Karolina/_load_karolina_modules.sh
+++ CG/batch/Karolina/_load_karolina_modules.sh
@@ -1,3 +1,7 @@
 ml NVSHMEM/2.9.0-gompi-2022a-CUDA-11.7.0
-ml Python/3.10.4-GCCcore-11.3.0
+ml Python/3.9.5-GCCcore-10.3.0
+ml PETSc/3.17.4-foss-2021a
+
+export PETSC_DIR=/apps/all/PETSc/3.17.4-foss-2021a
 export NVSHMEM_IB_ENABLE_IBGDA=true
+export PYTHONPATH=$PETSC_DIR/lib/petsc/bin/:$PYTHONPATH
